3726f3de834c0ed9de7a07dba67ea11ceb07bea2
EricGebhart <e.a.gebhart@gmail.com>
2016-06-22 16:31:35 -0400

    get a newer potempkin. Clojure 1.7.

--- project.clj
+++ project.clj
@@ -3,9 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                                         ;[org.clojure/clojure "1.8.0"]
-                 [cascalog/cascalog-core "3.0.0"]]
+                 [cascalog/cascalog-core "3.0.0"
+                  :exclude [[prismatic/schema "0.3.7"]
+                                        ;prismatic/schema has [potempkin "0.3.2"]
+                                        ; this is bad for eastwood.
+                            ]]
+                 ;;[potemkin "0.4.3"]
+                 [prismatic/schema "1.1.2"] ;; has potemkin 0.4.1
+
+                 ]
 
   :plugins [[lein-environ "1.0.0"]
             [lein-typed "0.3.5"]]
@@ -22,7 +30,7 @@
                    :dependencies [[midje "1.8.3"]
                                   [org.apache.hadoop/hadoop-core "1.2.1"]]
 
-                   :env {:squiggly {:checkers [:eastwood :kibit]
+                   :env {:squiggly {:checkers [:kibit] ;; :eastwood
                                     :eastwood-exclude-linters [:unlimited-use]
                                     :eastwoood-options {;; :bultin-config-files ["myconfigfile.clj"]
                                                         }}}}})
